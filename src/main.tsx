ad6ec539a0c1bebf90d355815d5c8da5d6de3c8e
Ekaterina Ivanova <ekaterinaivanova@MacBook-Air-Ekaterina.local>
2025-01-18 17:37:59 +0300

    routes fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Home } from './pages/Home.tsx'
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
 		<Routes>
-			<Route path='/' element={<App />}>
+			<Route path='frontend-challenge' element={<App />}>
 				<Route index element={<Home />} />
 				<Route path='favorites' element={<Favorites />} />
 			</Route>
